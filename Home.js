47f573233a02ddcfd3b28d1d81f8fb877cbd3190
isurusiri <isuruanatomy@gmail.com>
2018-09-07 23:36:53 +0530

    added some swag :rocket: :fire: :sunglasses: :cloud: :umbrella:

--- Home.js
+++ Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList, Text, View, StyleSheet } from 'react-native';
+import { FlatList, Text, View, StyleSheet, Image } from 'react-native';
 
 import { getWeather } from './api';
 import WeatherCard from './WeatherCard';
@@ -16,6 +16,15 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: 20,
         backgroundColor: '#F3F3F3'
+    },
+    animationContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F3F3F3'
+    },
+    animation: {
+        width: 150,
+        height: 150
     }
 });
 
@@ -39,14 +48,18 @@ class Home extends Component {
 
     render() {
 
-        return (
+        return [
             <FlatList 
                 style={styles.list}
                 data={this.state.weatherData}
                 renderItem={({ item }) => <WeatherCard weatherItem={item} />}
                 keyExtractor={item => item.temperatureC.toString()}
-            />
-        );
+                key="listfield"
+            />,
+            <View key="animcontainer" style={styles.animationContainer} >
+                <Image  style={styles.animation} source={require('./Weather-Umbrella.gif')}/>
+            </View>
+        ];
     }
 }
 
